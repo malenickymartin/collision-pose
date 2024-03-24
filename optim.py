rename vars in change perc frame, fix perc jacobian and residual, better vis

optim.py
@@ -7,28 +7,28 @@ import copy
 
 def change_Q_frame(var_xy_z_theta: List, M: pin.SE3) -> np.ndarray:
 
-    t_o = M.translation
-    R_o = M.rotation
+    t_c_o = M.translation
+    rot_c_o = M.rotation
 
     var_xy, var_z, var_theta = var_xy_z_theta
 
-    cov_trans_cam_aligned = np.diag([var_xy, var_xy, var_z])
-    t_o_norm = t_o/np.linalg.norm(t_o)
+    cov_trans_cp = np.diag([var_xy, var_xy, var_z])
+    t_o_norm = t_c_o/np.linalg.norm(t_c_o)
     v = np.cross([0, 0, 1], t_o_norm)
     ang = np.arccos(np.dot([0, 0, 1], t_o_norm))
     v_norm = np.linalg.norm(v)
     if v_norm > 1e-6:
         v = v/v_norm
-    rot = pin.exp3(ang * v)
-    cov_trans_o = rot @ cov_trans_cam_aligned @ rot.T  # cov[AZ] = A cov[Z] A^T
-    #rot = R_o.T
-    #cov_trans_o = rot @ cov_trans_c @ rot.T
+    rot_c_cp = pin.exp3(ang * v)
+    cov_trans_c = rot_c_cp @ cov_trans_cp @ rot_c_cp.T  # cov[AZ] = A cov[Z] A^T
+    
+    cov_rot_c = rot_c_o @ np.diag([var_theta] * 3) @ rot_c_o.T
 
-    cov_o = np.zeros((6, 6))
-    cov_o[:3, :3] = cov_trans_o
-    cov_o[3:6, 3:6] = np.diag([var_theta] * 3)
+    cov_c = np.zeros((6, 6))
+    cov_c[:3, :3] = cov_trans_c
+    cov_c[3:6, 3:6] = cov_rot_c
 
-    return cov_o
+    return cov_c
 
 def std_to_Q_aligned(std_xy_z_theta: List[float], Mm: pin.SE3) -> np.ndarray:
     """
@@ -125,11 +125,10 @@ def perception_res_grad(M_lst: List[pin.SE3], Mm_lst: List[pin.SE3], L_lst: List
     res = np.zeros(6*N)
     for i in range(N):
         L = L_lst[i]
-        e, J = error_fun(M_lst[i], Mm_lst[i])
-        r = L @ e
+        e, J = error_fun(M_lst[i], Mm_lst[i], True)
+        r = L.T @ e
         g = r @ L.T @ J
         res[6*i:6*i+6], grad[6*i:6*i+6] = r, g
-        # res[6*i:6*i+6], grad[6*i:6*i+6] = res_grad_se3(M_lst[i], Mm_lst[i], L_lst[i])
     
     return res, grad
 

scripts/cov.py
@@ -13,6 +13,7 @@ def meshcat_material(r, g, b, a):
     return material
 
 def get_ellipsoid(cov, nstd=2):
+    # https://users.cs.utah.edu/~tch/CS4640/resources/A%20geometric%20interpretation%20of%20the%20covariance%20matrix.pdf
     eigvals, eigvecs = np.linalg.eigh(cov)
     radii = np.sqrt(eigvals) * nstd
     return radii, eigvecs
@@ -55,7 +56,7 @@ def test_change_frame():
         # Generate random poses
         poses = [pin.SE3.Random() for _ in range(len(meshes))]
         for pose in poses:
-            pose.translation[:2] = 1
+            pose.translation[2] = 1
     
     covs = [change_Q_frame(Q, pose) for pose in poses]
 
@@ -72,7 +73,7 @@ if __name__ == "__main__":
     i = int(input("Press 1 to test_change_frame, 2 to plot points and ellipsoid: "))
     if i == 1:
         test_change_frame()
-    else:
+    elif i == 2:
         vis = meshcat.Visualizer(zmq_url="tcp://127.0.0.1:6000")
         vis.delete()
 
@@ -85,7 +86,7 @@ if __name__ == "__main__":
         for T in points_T:
             T.translation = np.random.multivariate_normal(mean_gt, cov_gt)
         for i, T in enumerate(points_T):
-            vis[f"point_{i}"].set_object(point_mesh, meshcat_material(*RED))
+            vis[f"point_{i}"].set_object(point_mesh, meshcat_material(*GREEN))
             vis[f"point_{i}"].set_transform(T.homogeneous)
 
 

test_numdiff_perception.py
@@ -13,22 +13,22 @@ def num_diff_perc(M: pin.SE3, Mm: pin.SE3, err=error_se3, exp=pin.exp6, EPS=1e-6
     
     return J
 
+for _ in range(1000):
+    M, Mm = pin.SE3.Random(), pin.SE3.Random()
 
-M, Mm = pin.SE3.Random(), pin.SE3.Random()
+    # SE3
+    J_n = num_diff_perc(M, Mm, err=error_se3)
+    e, J = error_se3(M, Mm, jac=True)
 
-# SE3
-J_n = num_diff_perc(M, Mm, err=error_se3)
-e, J = error_se3(M, Mm, jac=True)
+    assert np.allclose(error_se3(M,M), np.zeros(6), atol=1e-5)
+    assert np.allclose(J, J_n, atol=1e-5)
 
-assert np.allclose(error_se3(M,M), np.zeros(6), atol=1e-7)
-assert np.allclose(J, J_n, atol=1e-7)
+    # R3xSO3
+    J_n = num_diff_perc(M, Mm, err=error_r3_so3)
+    e, J = error_r3_so3(M, Mm, jac=True)
 
-# R3xSO3
-J_n = num_diff_perc(M, Mm, err=error_r3_so3)
-e, J = error_r3_so3(M, Mm, jac=True)
-
-assert np.allclose(error_r3_so3(M,M), np.zeros(6), atol=1e-7)
-assert np.allclose(J, J_n, atol=1e-7)
+    assert np.allclose(error_r3_so3(M,M), np.zeros(6), atol=1e-5)
+    assert np.allclose(J, J_n, atol=1e-5)
 
 
 